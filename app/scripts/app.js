11ea7ae54b1f851c3b31377f3650b2ee5557aa90
jlea-jones <jlea-jones@about.com>
2015-03-09 19:41:52 -0400

    setup routes, tests and views, and main navigation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,12 +21,48 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
         controller: 'AboutCtrl'
+      })
+      .when('/services', {
+        templateUrl: 'views/services.html',
+        controller: 'ServicesCtrl'
+      })
+      .when('/comedy', {
+        templateUrl: 'views/comedy.html',
+        controller: 'ComedyCtrl'
+      })
+      .when('/sports', {
+        templateUrl: 'views/sports.html',
+        controller: 'SportsCtrl'
+      })
+      .when('/concerts', {
+        templateUrl: 'views/concerts.html',
+        controller: 'ConcertsCtrl'
+      })
+      .when('/events', {
+        templateUrl: 'views/events.html',
+        controller: 'EventsCtrl'
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl'
+      })
+      .when('/services/marketing', {
+        templateUrl: 'views/marketing.html',
+        controller: 'MarketingCtrl'
+      })
+      .when('/services/event-planning', {
+        templateUrl: 'views/eventplanning.html',
+        controller: 'EventplanningCtrl'
+      })
+      .when('/services/booking', {
+        templateUrl: 'views/booking.html',
+        controller: 'BookingCtrl'
       })
       .otherwise({
         redirectTo: '/'
